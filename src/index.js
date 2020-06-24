c447c12a98afadfd400c8c2adb7fa86773e20800
John Sylvain <john.n.sylvain@gmail.com>
2020-06-24 16:10:57 -0500

    refactor client code

--- src/index.js
+++ src/index.js
@@ -1,39 +1,92 @@
-import { animate, poll } from "./util";
+const domElements = {
+  toggle: document.querySelector("#toggle"),
+  volume: document.querySelector("#volume"),
+  audio: document.querySelector("#audio"),
+  song: document.querySelector("#song"),
+  artist: document.querySelector("#artist"),
+  listeners: document.querySelector("#listeners"),
+  animation: document.querySelector("#animation"),
+  cycle: document.querySelector("#cycle"),
+};
 
-const $toggle = document.querySelector("#toggle");
-const $volume = document.querySelector("#volume");
-const $audio = document.querySelector("#audio");
-const $song = document.querySelector("#song");
-const $artist = document.querySelector("#artist");
-const $listeners = document.querySelector("#listeners");
+const appState = {
+  isPlaying: false,
+  isLoading: true,
+};
 
-const animations = {
+const animate = (callback, timeout) => {
+  let id;
+
+  function loop() {
+    id = setTimeout(loop, timeout);
+    callback();
+  }
+
+  loop();
+
+  return () => clearTimeout(id);
+};
+
+const poll = (url, timeout, cb) => {
+  const fn = async () => {
+    const blob = await fetch(url);
+    const json = await blob.json();
+    cb(json);
+  };
+
+  return animate(fn, timeout);
+};
+
+const createAnimationManager = () => ({
   unsubscribe: undefined,
   animations: {},
-  register: function (name, a) {
-    this.animations[name] = a;
+  register: function (name, fn) {
+    this.animations[name] = fn;
   },
   trigger: function (name, data) {
     if (this.unsubscribe) this.unsubscribe();
     this.unsubscribe = this.animations[name](data);
   },
-};
+});
+
+const musicPlayerAnimations = createAnimationManager();
+const asciiAnimations = createAnimationManager();
 
 function animateLoading() {
   let i = 0;
-  const flipFrames = ["-", "\\", "|", "/"]
+  const flipFrames = ["-", "\\", "|", "/"];
+
+  function renderLoadingBar(i) {
+    const text = " Loading Stream ";
+    const bar = "░".repeat(20);
+    const position = bar.length - (i % (5 + bar.length + text.length));
+    return (
+      bar.substring(0, position) +
+      text.substring(Math.abs(Math.min(0, position)))
+    )
+      .substring(0, 20)
+      .padEnd(20, "░");
+  }
 
   return animate(() => {
-    $volume.textContent = renderLoadingBar(i);
-    $toggle.textContent = flipFrames[i % flipFrames.length]
-    $toggle.disabled = true;
+    domElements.volume.textContent = renderLoadingBar(i);
+    domElements.toggle.textContent = flipFrames[i % flipFrames.length];
+    domElements.toggle.disabled = true;
     i++;
   }, 150);
 }
 
 function animateSound({ analyser, frequencyData }) {
-  $toggle.textContent = "❙❙";
-  $toggle.disabled = false;
+  domElements.toggle.textContent = "❙❙";
+  domElements.toggle.disabled = false;
+
+  function renderVolumeMeter(value = 0) {
+    const length = 20;
+    const percent = value / 100;
+    const clamped = percent > 1 ? 1 : percent;
+    return "█".repeat(Math.floor(clamped * length)).padEnd(20, "░");
+  }
+
   function loop() {
     requestAnimationFrame(loop);
     analyser.getByteFrequencyData(frequencyData);
@@ -42,111 +95,70 @@ function animateSound({ analyser, frequencyData }) {
     const bands = Array.from({ length: numberOfBands })
       .map((_, i) => frequencyData.slice(i * bandWidth, (i + 1) * bandWidth))
       .map((data) => data.reduce((a, c) => a + c, 0) / bandWidth)
-      .reduce((res, band) => res + renderVolumeMeter(band, "horizontal"), "");
-    $volume.textContent = bands;
+      .reduce((res, band) => res + renderVolumeMeter(band), "");
+    domElements.volume.textContent = bands;
   }
   loop();
-  return function () { };
+  return function () {};
 }
 
-animations.register("loading", animateLoading);
-animations.register("analyse", animateSound);
+musicPlayerAnimations.register("loading", animateLoading);
+musicPlayerAnimations.register("analyse", animateSound);
+asciiAnimations.register("play", (frames) => {
+  let frame = 0;
+  domElements.animation.textContent = frames[0];
+  return animate(() => {
+    frame = (frame + 1) % frames.length;
+    domElements.animation.textContent = frames[frame];
+  }, 500);
+});
+
+function createAnalyser($audio) {
+  const context = new (typeof window.webkitAudioContext !== "undefined"
+    ? window.webkitAudioContext
+    : window.AudioContext)();
+  const audioSrc = context.createMediaElementSource($audio);
+  const analyser = context.createAnalyser();
+  audioSrc.connect(analyser);
+  analyser.connect(context.destination);
+  return analyser;
+}
 
 function analyseAudio($audio) {
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const ctx = new AudioContext();
-  const audioSrc = ctx.createMediaElementSource($audio);
-  const analyser = ctx.createAnalyser();
-  audioSrc.connect(analyser);
+  const analyser = createAnalyser($audio);
   const frequencyData = new Uint8Array(analyser.frequencyBinCount);
-  analyser.connect(ctx.destination);
 
   const id = setInterval(() => {
     analyser.getByteFrequencyData(frequencyData);
     const sum = frequencyData.reduce((a, c) => a + c, 0);
     if (sum > 0) {
       clearInterval(id);
-      animations.trigger("analyse", { analyser, frequencyData });
+      musicPlayerAnimations.trigger("analyse", { analyser, frequencyData });
     }
   }, 1000);
 }
 
-function renderVolumeMeter(value = 0, layout = "vertical") {
-  if (layout === "vertical") {
-    const bars = ["▁", "▂", "▃", "▅", "▆", "▇", "█"];
-    const percent = value / 100;
-    const clamped = percent > 1 ? 1 : percent;
-    return bars[Math.floor(clamped * bars.length - 1)] || " ";
+domElements.volume.textContent = "░".repeat(20);
+
+domElements.toggle.addEventListener("click", () => {
+  appState.isPlaying = !appState.isPlaying;
+
+  if (appState.isPlaying) {
+    if (!domElements.audio.getAttribute("src")) {
+      domElements.audio.setAttribute("src", "/stream");
+    }
+    if (appState.isLoading) {
+      appState.isLoading = false;
+      musicPlayerAnimations.trigger("loading");
+      analyseAudio(domElements.audio);
+    }
+    domElements.audio.play();
+    domElements.toggle.textContent = "❙❙";
   } else {
-    const length = 20;
-    const percent = value / 100;
-    const clamped = percent > 1 ? 1 : percent;
-    return "█".repeat(Math.floor(clamped * length)).padEnd(20, "░");
-  }
-}
-
-function renderLoadingBar(i) {
-  const text = " Loading Stream ";
-  const bar = "░".repeat(20);
-  const position = bar.length - i % (5 + bar.length + text.length);
-  return (
-    bar.substring(0, position) + text.substring(Math.abs(Math.min(0, position)))
-  )
-    .substring(0, 20)
-    .padEnd(20, "░");
-}
-
-const pre = document.createElement("pre");
-document.querySelector("#animation").appendChild(pre);
-$volume.textContent = "░".repeat(20);
-
-let playing = false;
-let loading = true;
-
-poll("/api", 7000, (data) => {
-  $song.textContent = data.title;
-  $artist.textContent = data.artist;
-  $listeners.textContent = `listeners: ${data.listeners}`;
-  const title = `Lounge FM - ${data.title} - ${data.artist}`;
-  if (document.title !== title) {
-    document.title = title;
+    domElements.audio.pause();
+    domElements.toggle.textContent = "▶";
   }
 });
-
-$toggle.addEventListener("click", () => {
-  playing = !playing;
-
-  if (playing) {
-    if (!$audio.getAttribute("src")) {
-      $audio.setAttribute("src", "/stream");
-    }
-    if (loading) {
-      loading = false;
-      animations.trigger("loading");
-      analyseAudio($audio);
-    }
-    $audio.play();
-    $toggle.textContent = "❙❙";
-  } else {
-    $audio.pause();
-    $toggle.textContent = "▶";
-  }
-});
-
-const render = (() => {
-  let unsubscribe;
-
-  return function (frames) {
-    if (unsubscribe) unsubscribe();
-
-    let frame = 0;
-    pre.textContent = frames[0];
-    unsubscribe = animate(() => {
-      frame = (frame + 1) % frames.length;
-      pre.textContent = frames[frame];
-    }, 500);
-  };
-})();
 
 const urls = [
   require("./scenes/bubbles.txt"),
@@ -161,11 +173,21 @@ Promise.all(urls.map(map)).then((s) => {
   let scene = window.localStorage.getItem("scene") || 0;
   const scenes = s.map((s) => s.split("<<<FRAME>>>"));
 
-  document.querySelector("#cycle").addEventListener("click", () => {
+  domElements.cycle.addEventListener("click", () => {
     scene = (parseInt(scene) + 1) % scenes.length;
     window.localStorage.setItem("scene", scene);
-    render(scenes[scene]);
+    asciiAnimations.trigger("play", scenes[scene]);
   });
 
-  render(scenes[scene]);
+  asciiAnimations.trigger("play", scenes[scene]);
+});
+
+poll("/api", 7000, (data) => {
+  domElements.song.textContent = data.title;
+  domElements.artist.textContent = data.artist;
+  domElements.listeners.textContent = `listeners: ${data.listeners}`;
+  const title = `Lounge FM - ${data.title} - ${data.artist}`;
+  if (document.title !== title) {
+    document.title = title;
+  }
 });
