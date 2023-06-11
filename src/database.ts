4eb580df8f8f383fd07ff94962c5ea2640e0ab34
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-06-11 22:01:04 +0200

    update

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
-import pino from 'pino';
-import * as dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import pino from 'pino'
+import * as dotenv from 'dotenv'
 dotenv.config()
-const logger = pino({ level: 'info' });
+const logger = pino({ level: 'info' })
 
-(async () => {
-  try {
-    const db = await mongoose.connect(`${process.env.MONGO}`);
-    console.log("Database is connected to: ", db.connection.name);
-  } catch (error) {
-    console.log("Databse Error")
-    console.error(error);
-  }
-})();
+;(async () => {
+    try {
+        const db = await mongoose.connect(`${process.env.MONGO}`)
+        console.log('Database is connected to: ', db.connection.name)
+    } catch (error) {
+        console.log('Databse Error')
+        console.error(error)
+    }
+})()
